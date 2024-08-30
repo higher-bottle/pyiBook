4fd6872f978e1fad521e2e34fb87d8a100360c0b
Tess HF <101187655+higher-bottle@users.noreply.github.com>
2024-08-31 02:35:20 +0800

    1st

--- setup.py
+++ setup.py
@@ -1,3 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="pyiBook",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'rich',
+        'pandas',
+        'requests',
+        'openpyxl'
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyiBook = pyiBook.__init__:cli',
+        ],
+    },
+    author="Tess",
+    description="A CLI tool for interacting with iBooks library.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/higher-bottle/pyiBook",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
